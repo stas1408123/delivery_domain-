e04220e3227a610fb57358affe7c7049850e0838
BeLove <>
2026-01-05 21:30:00 +0300

    Added baseEventSourcedAggregate

---
+++ src/Ordering.Domain/Common/BaseEventSourcedAggregate.cs
@@ -0,0 +1,31 @@
+﻿namespace Ordering.Domain.Common
+{
+    public abstract class BaseEventSourcedAggregate : IEventSourcedAggregate
+    {
+        public Guid Id { get; protected set; }
+        public int Version { get; protected set; } = 0;
+
+        private readonly List<IEvent> _uncommittedEvents = new List<IEvent>();
+
+        public IReadOnlyCollection<IEvent> GetUncommittedEvents() => _uncommittedEvents.AsReadOnly();
+
+        public void ClearUncommittedEvents() => _uncommittedEvents.Clear();
+
+        public void Load(IEnumerable<IEvent> history)
+        {
+            foreach (var @event in history.OrderBy(e => e.AggregateVersion))
+            {
+                Apply(@event);
+            }
+        }
+
+        public void AppendEvent(IEvent @event)
+        {
+            Version = @event.AggregateVersion++;
+            Apply(@event);
+            _uncommittedEvents.Add(@event);
+        }
+
+        public abstract void Apply(IEvent @event);
+    }
+}
