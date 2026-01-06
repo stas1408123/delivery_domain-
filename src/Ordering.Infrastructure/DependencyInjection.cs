bb15f25f3ba7e129bae56aeb3a915b8b782631a1
BeLove <>
2026-01-07 02:03:00 +0300

    Added Order Repository

--- src/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Ordering.Infrastructure/DependencyInjection.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ordering.Application.Common.Interfaces;
 using Ordering.Application.Services;
+using Ordering.Domain.AggregatesModels.OrderAggregate;
 using Ordering.Domain.Common;
+using Ordering.Infrastructure.Cache;
 using Ordering.Infrastructure.EventStore;
 using Ordering.Infrastructure.Repositories;
 using Ordering.Infrastructure.Serializer;
@@ -17,6 +19,13 @@ namespace Ordering.Infrastructure
         {
             builder.Services.AddScoped<IEventStoreSerializer, EventStoreJsonSerializer>();
             builder.Services.AddScoped<IEventStore, EventStoreRepository>();
+            builder.Services.AddMemoryCache();
+
+            builder.Services.AddSingleton<ICacheRepository, MemoryCacheRepository>();
+            builder.Services.AddSingleton<ISnapshotCache<OrderSnapshot>, SnapshotCache<OrderSnapshot>>();
+            builder.Services.AddScoped(typeof(IEventSourcedRepository<Order>), typeof(OrderRepository));
+
+
             builder.Services.AddScoped(typeof(IEventSourcedRepository<>), typeof(EventSourcedRepository<>));
             builder.Services.AddDbContext<OrderingDbContext>((options) =>
             {
