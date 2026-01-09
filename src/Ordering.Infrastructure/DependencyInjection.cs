a0223dfdf4731c1757ef263323f7c048c0023360
BeLove <>
2026-01-09 23:01:59 +0300

    Added read models repos

--- src/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Ordering.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ordering.Application.Common.Interfaces;
+using Ordering.Application.ReadModels.Orders.Models;
 using Ordering.Application.Services;
 using Ordering.Domain.AggregatesModels.OrderAggregate;
 using Ordering.Domain.Common;
@@ -24,7 +25,7 @@ namespace Ordering.Infrastructure
             builder.Services.AddSingleton<ICacheRepository, MemoryCacheRepository>();
             builder.Services.AddSingleton<ISnapshotCache<OrderSnapshot>, SnapshotCache<OrderSnapshot>>();
             builder.Services.AddScoped(typeof(IEventSourcedRepository<Order>), typeof(OrderRepository));
-
+            builder.Services.AddScoped<IOrderReadModelRepository, OrderReadModelRepository>();
 
             builder.Services.AddScoped(typeof(IEventSourcedRepository<>), typeof(EventSourcedRepository<>));
             builder.Services.AddDbContext<OrderingDbContext>((options) =>
