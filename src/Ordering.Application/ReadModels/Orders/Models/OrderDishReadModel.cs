34475abe28a5c54f61e053f0f74cf75b6d28954f
BeLove <>
2026-01-09 23:00:12 +0300

    Added basic read models

---
+++ src/Ordering.Application/ReadModels/Orders/Models/OrderDishReadModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ordering.Application.ReadModels.Orders.Models
+{
+    public class OrderDishReadModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid OrderReadId { get; set; }
+        public OrderReadModel? OrderReadModel { get; set; }
+        public Guid ProductId { get; set; }
+        public int Amount { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
---
+++ src/Ordering.Application/ReadModels/Orders/Models/OrderReadModel.cs
@@ -0,0 +1,16 @@
+﻿using Ordering.Domain.AggregatesModels.OrderAggregate;
+
+namespace Ordering.Application.ReadModels.Orders.Models
+{
+    public class OrderReadModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public decimal TotalAmount { get; set; }
+        public int ItemCount { get; set; }
+
+        public List<OrderDishReadModel> Dishes { get; set; } = new();
+    }
+}
